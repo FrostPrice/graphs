b6acb06a02149e764ea406a8a739dc3cc06e57d9
mateusbarbosa <barbosamat16@gmail.com>
2025-03-31 10:55:19 -0300

    Refactor: Move main function to separate source file and update file handling

---
+++ m1-2/src/main.cpp
@@ -0,0 +1,28 @@
+#include "Graph.hpp"
+#include "Utils.hpp"
+#include "NavAlgorithms.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Uso: " << argv[0] << " <arquivo_grafo>\n";
+        return 1;
+    }
+
+    std::string filename = argv[1];
+
+    Graph *g = loadGraphFromFile(filename, RepresentationType::LIST); // or MATRIX
+
+    if (g)
+    {
+        g->printGraph();
+        bfs(*g, "0");
+        dfs(*g, "0");
+        dijkstra(*g, "0");
+        delete g;
+    }
+
+    return 0;
+}
